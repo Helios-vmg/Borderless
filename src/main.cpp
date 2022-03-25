5b362013706f78b84bf205c6cf04e31065f7b732
Víctor M. González <helios.vmg@gmail.com>
2022-03-25 08:58:38 -0300

    Increase the QImageReader allocation limit.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@ Distributed under a permissive license. See COPYING.txt for details.
 */
 
 #include "ImageViewerApplication.h"
+#include <QImageReader>
 
 int main(int argc, char **argv){
 	try{
+		//Set the limit to 1 GiB.
+		QImageReader::setAllocationLimit(1024);
 		initialize_supported_extensions();
 		ImageViewerApplication app(argc, argv, "BorderlessViewer" + get_per_user_unique_id());
 		return app.exec();
